084c4c2037fa395334e0acb761096896d16a5ebf
hoyeon joo <jooc12@naver.com>
2022-04-22 17:09:16 +0900

    Add Vue Project

---
+++ front-end/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <SignIn msg=""/>
+  </div>
+</template>
+
+<script>
+import SignIn from './components/SignIn.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SignIn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #da7110;
+  margin-top:-8px;
+  margin-left: -8px;
+}
+</style>
